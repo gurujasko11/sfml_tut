995e3aa2a9a595b4c4d3589b2e2aa866c0cff8be
gurujasko11 <gurujasko11@gmail.com>
2017-05-19 21:32:28 +0200

    get_enemy_in_range and fixed the name

---
+++ headers/ObjectStorage.h
@@ -0,0 +1,23 @@
+#ifndef SFML_TUT_OBJECTSSTORAGE_H
+#define SFML_TUT_OBJECTSSTORAGE_H
+
+#include "Enemy.h"
+#include "Turret.h"
+
+class ObjectStorage {
+private:
+		std::list<Enemy*> enemies;
+		std::list<Turret*> turrets;
+public:
+		void add_enemy(Enemy* enemy);
+		void add_turrets(Turret* turret);
+
+		std::list<Enemy*>::iterator get_enemies_begin();
+		std::list<Enemy*>::iterator get_enemies_end();
+
+		std::list<Turret*>::iterator get_turrets_begin();
+		std::list<Turret*>::iterator get_turrets_end();
+
+		std::list<Enemy*> get_enemy_in_range(sf::Shape* shape, float range);
+};
+#endif //SFML_TUT_OBJECTSSTORAGE_H
---
+++ source/ObjectStorage.cpp
@@ -0,0 +1,32 @@
+#include "../headers/Enemy.h"
+#include "../headers/ObjectStorage.h"
+
+void ObjectStorage::add_enemy (Enemy* enemy) {
+	enemies.push_back(enemy);
+}
+void ObjectStorage::add_turrets(Turret* turret) {
+	turrets.push_back(turret);
+}
+
+std::list<Enemy*>::iterator ObjectStorage::get_enemies_begin() {
+	return enemies.begin();
+}
+std::list<Enemy*>::iterator ObjectStorage::get_enemies_end() {
+	return enemies.end();
+}
+
+std::list<Turret*>::iterator ObjectStorage::get_turrets_begin() {
+	return turrets.begin();
+}
+std::list<Turret*>::iterator ObjectStorage::get_turrets_end() {
+	return turrets.end();
+}
+
+std::list<Enemy*> ObjectStorage::get_enemy_in_range(sf::Shape* shape, float range) {
+	std::list<Enemy*> result;
+	for(std::list<Enemy*>::iterator i; i != enemies.end(); ++i) {
+		if((*i)->get_distance_from(shape) < range)
+			result.push_back(*i);
+	}
+	return result;
+}
